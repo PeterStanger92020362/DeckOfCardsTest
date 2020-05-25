ba18f815cc964853184b0ded1cfb4635238071cf
Peter Stanger <psta191@mywhitecliffe.com>
2020-05-25 19:54:08 +1200

    Initial Commit

---
+++ src/deckofcardstest/DeckOfCardsTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deckofcardstest;
+
+/**
+ *
+ * @author Peter
+ */
+public class DeckOfCardsTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        DeckOfCards myDeckOfCards = new DeckOfCards();
+        myDeckOfCards.shuffle();
+        
+        for ( int i = 0; i < 13; i++){
+            
+            System.out.printf( "%-20s%-20s%-20s%-20s\n", 
+                    myDeckOfCards.dealCard(), myDeckOfCards.dealCard(),
+                    myDeckOfCards.dealCard(), myDeckOfCards.dealCard() );
+        }
+        
+    }
+    
+}
